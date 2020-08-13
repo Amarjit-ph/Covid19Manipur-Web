711011df29c89f98c26caed15390b86fee8257fe
Amarjit-pheiroijam <spartanjaggerz@gmail.com>
2020-08-13 22:24:14 +0530

    13-Aug-2020

--- src/app.js
+++ src/app.js
@@ -9,17 +9,17 @@ const App = () => {
     let Data;
 
     const Static = {
-        S_active: 1739,
-        S_confirmed: 3982,
-        S_recovered: 2231,
-        S_dead: 12,
-        S_tested: 104268 + 2038,
+        S_active: 1804,
+        S_confirmed: 4112,
+        S_recovered: 2295,
+        S_dead: 13,
+        S_tested: 107469,
         daily: true,
 
-        SD_confirmed: 34,
-        SD_recovered: 76,
-        SD_dead: 0,
-        SD_tested: 2058,
+        SD_confirmed: 130,
+        SD_recovered: 64,
+        SD_dead: 1,
+        SD_tested: 16701,
 
 
         Q_completed: 20178 + 39457 + 2947,
@@ -337,7 +337,7 @@ const App = () => {
                 Static.daily ?
                     <div>
                         <div className='district_head'>
-                            <p><b>DATE<br />12-8-2020</b></p>
+                            <p><b>DATE<br />13-8-2020</b></p>
                             <br />
                             <h4>Daily Updates are performed<br />when the Data are Available.</h4>
                         </div>
